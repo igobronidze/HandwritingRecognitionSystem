2417cb315a900c3cc420cb445c3e65affca0859b
Ioseb Gobronidze <gobronidze.ioseb@gmail.com>
2017-02-13 22:49:54 +0400

    added opencv dependencies...

---
+++ image_processing/src/test/java/ge/edu/tsu/hcrs/image_processing/OpenCVTest.java
@@ -0,0 +1,19 @@
+package ge.edu.tsu.hcrs.image_processing;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+public class OpenCVTest {
+
+    @Test
+    @Ignore
+    public void testOpenCV() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
+    }
+}
