2eed695982fd7742eadd38831782c0f494852d65
Ilia Kapanadze <ilia.kapanadze1@gmail.com>
2017-02-26 00:24:30 +0400

    created system parameters page

--- control_panel/console/src/main/java/ge/edu/tsu/hcrs.control_panel.console/SystemParametersController.java
+++ control_panel/console/src/main/java/ge/edu/tsu/hcrs.control_panel.console/SystemParametersController.java
@@ -1,35 +1,42 @@
 package ge.edu.tsu.hcrs.control_panel.console;
 
 import ge.edu.tsu.hcrs.control_panel.model.exception.ControlPanelException;
+import ge.edu.tsu.hcrs.control_panel.model.sysparam.SysParamType;
 import ge.edu.tsu.hcrs.control_panel.model.sysparam.SystemParameter;
 import ge.edu.tsu.hcrs.control_panel.service.systemparameter.SystemParameterService;
 import ge.edu.tsu.hcrs.control_panel.service.systemparameter.SystemParameterServiceImpl;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.util.Map;
 
-@RestController
+@Controller
 public class SystemParametersController {
 
     private SystemParameterService systemParameterService = new SystemParameterServiceImpl();
 
-    @RequestMapping(value = "/sys_param/add", method = RequestMethod.PUT)
-    public SystemParameter addSystemParameter(@RequestParam(value="key") String key,
-                                              @RequestParam(value="value") String value) throws ControlPanelException {
-        SystemParameter systemParameter = new SystemParameter(key, value);
-        systemParameterService.addSystemParameter(systemParameter);
-        return systemParameter;
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public String sysparams(Map<String, Object> model) {
+        model.put("systemParameters", systemParameterService.getSystemParameters(null, null));
+        model.put("sysParamTypes", SysParamType.values());
+        return "sysparams";
     }
 
-    @RequestMapping(value = "/sys_param", method = RequestMethod.DELETE)
-    @ResponseStatus(value = HttpStatus.OK)
-    public void getSystemParameterById(@RequestParam("key") String key) throws ControlPanelException {
-        systemParameterService.deleteSystemParameter(key);
-    }
-
-    @RequestMapping(value = "/sys_param/all", method = RequestMethod.GET)
-    public List<SystemParameter> getAllSystemParameters() {
-        return systemParameterService.getSystemParameters(null, null);
+    @RequestMapping(value = "/addSysParam", method=RequestMethod.POST)
+    public String addSysParam(@RequestParam("key") String key,
+                              @RequestParam("value") String value,
+                              @RequestParam("type") String type) {
+        SystemParameter systemParameter = new SystemParameter();
+        systemParameter.setKey(key);
+        systemParameter.setValue(value);
+        systemParameter.setType(SysParamType.valueOf(type));
+        try {
+            systemParameterService.addSystemParameter(systemParameter);
+        } catch (ControlPanelException e) {
+            System.out.println(e.getMessage());
+        }
+        return "redirect:";
     }
 }
